7daf49eb412d802e83654a76152e23b0308d234a
Lauren Hartley <lahartley99@gmail.com>
2022-11-04 14:11:14 -0400

    create html and data for competition page

--- scripts/createCompetition.js
+++ scripts/createCompetition.js
@@ -2,23 +2,23 @@
 const tableBody = $('#table-body');
 
 /**
- * Create a meeting row for a table.
+ * Create a competition row for a table.
  *
- * @param {*} meeting Meeting to create a row for.
+ * @param {*} comp Competition to create a row for.
  * @return {String} HTML string for a table row.
  */
-function createRow(meeting) {
+function createRow(comp) {
     const row = `
     <tr>
-    <td>${meeting.date}</td>
-    <td>${meeting.topic}</td>
-    <td>${meeting.slides}</td>
+    <td>${comp.date}</td>
+    <td>${comp.name}</td>
+    <td>${comp.location}</td>
     </tr>`;
     return row;
 }
 
 // Create a row for each meeting and append it to the table body.
-fetch("./scripts/data/meetings.json")
+fetch("./scripts/data/competitions.json")
     .then(response => response.json())
     .then(json => {
         for (let meeting of json) {
